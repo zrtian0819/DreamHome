cc04601add9204997e5da90687bfbf5e6db247bc
Joe_Zhang <cjt3399@gmail.com>
2023-10-09 17:20:51 +0800

    微調介面

--- all.js
+++ all.js
@@ -1,9 +1,9 @@
 var app = new Vue({
     el: '#app',
     data:{
-      welcomeWords: 'Welcome to「夢想家」',
+      welcomeWords: '歡迎來到「夢想家」。',
       Homephoto: 'materials/photos/IMG_5719.JPG',
-      introduction: "由我來幫您找一個家!",
+      introduction: "由我來幫您找一個家❤!",
       producer:"Ren Tina Zhang",
       },
 
@@ -25,7 +25,7 @@ var app = new Vue({
           if (window.innerWidth < 860) {
             this.welcomeWords = '「夢想家」';
           }else{
-            this.welcomeWords = 'Welcome to「夢想家」'
+            this.welcomeWords = '歡迎來到「夢想家」。'
           }
       }
 
