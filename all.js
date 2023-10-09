95f5fa96c6dbb12ffb768615e408414833f5d72d
Joe_Zhang <cjt3399@gmail.com>
2023-10-09 16:38:51 +0800

    adding RWD design

--- all.js
+++ all.js
@@ -1,8 +1,35 @@
 var app = new Vue({
     el: '#app',
     data:{
-      welcomeWords: '歡迎來到「夢想家」',
+      welcomeWords: 'Welcome to「夢想家」',
       Homephoto: 'materials/photos/IMG_5719.JPG',
-      introduction: "我幫您找一個家!"
+      introduction: "由我來幫您找一個家!",
+      producer:"Ren Tina Zhang",
+      },
+
+      watch: {
+        // 监听窗口大小的变化
+        '$route'() {
+          this.checkScreenWidth();
+        },
+      },
+
+      mounted(){
+        this.checkScreenWidth();
+        window.addEventListener('resize', this.checkScreenWidth);
+      },
+
+      methods:{
+        checkScreenWidth() {
+          // 根据窗口宽度决定标题内容
+          if (window.innerWidth < 860) {
+            this.welcomeWords = '「夢想家」';
+          }else{
+            this.welcomeWords = 'Welcome to「夢想家」'
+          }
+      }
+
+
+
     }
   })
