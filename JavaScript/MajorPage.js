2fbdcac89f99d86b2065e4f6b1b650c54ba9c6bb
Joe_Zhang <cjt3399@gmail.com>
2023-10-10 15:12:10 +0800

    主頁將標題按鈕加入Vue

---
+++ JavaScript/MajorPage.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    // 標題按鈕的Vue
+    el: '#linkList',
+    data:{
+      text:'testing',
+      linkButton:[
+        {
+            title:'找租屋',
+            link:''
+        },
+        {
+            title:'找房子',
+            link:''
+        },
+        {
+            title:'找土地',
+            link:''
+        },
+        {
+            title:'聯繫我',
+            link:''
+        }
+
+      ]
+    }
+  })
