e5ce05cdf46f1fd5ff8e55a5b922f631e135979c
Joe_Zhang <cjt3399@gmail.com>
2023-10-10 22:51:20 +0800

    新增底部的連結圖示

--- JavaScript/MajorPage.js
+++ JavaScript/MajorPage.js
@@ -3,7 +3,8 @@ var app = new Vue({
     el: '#linkList',
     data:{
       text:'testing',
-      linkButton:[
+      linkButton:
+      [
         {
             title:'找租屋',
             link:'#bottom'
@@ -17,11 +18,49 @@ var app = new Vue({
             link:'#bottom'
         },
         {
-            title:'聯繫我',
-            link:'#bottom'
+            title:'關於我',
+            link:'contact_me.html'
         }
+      ],
 
+    }
+  })
+
+
+
+  var app = new Vue({
+    // 標題按鈕的Vue
+    el: '#bottom',
+    data:{
+      linkPressWords:'想看更多?',
+      Icon:
+      [
+        {
+          name:'Facebook',
+          link:'https://www.facebook.com/groups/330793587320368',
+          src:'materials/icons/facebook_1384021.png',
+          display:true
+        },
+        {
+          name:'Instagram',
+          link:'',
+          src:'materials/icons/instagram_1384031.png',
+          display:false
+        },
+        {
+          name:'Youtube',
+          link:'https://www.youtube.com/channel/UCj9u9U6SbGLZZRZ86q3ZEhA',
+          src:'materials/icons/youtube_1384028.png',
+          display:true
+        },
+        {
+          name:'Tiktok',
+          link:'https://vt.tiktok.com/ZSNYAyfW3/',
+          src:'materials/icons/tik-tok_3046120.png',
+          display:true
+        },
       ]
+
     }
   })
 
