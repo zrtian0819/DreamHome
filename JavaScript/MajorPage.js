b043548d00120271ce09936751d9530c2b49e72b
Joe_Zhang <cjt3399@gmail.com>
2023-11-01 23:32:58 +0800

    大修正關於我頁面aboutme頁面

--- JavaScript/MajorPage.js
+++ JavaScript/MajorPage.js
@@ -1,89 +1,82 @@
 var app = new Vue({
-    // 標題按鈕的Vue
-    el: '#linkList',
-    data:{
-      text:'testing',
-      linkButton:
-      [
-        {
-          title:'關於我',
-          link:'contact_me.html',
-          style:"btn link-active"
-        },
-        {
-            title:'找租屋',
-            link:'#bottom',
-            style:"btn"
-        },
-        {
-            title:'找房子',
-            link:'#bottom',
-            style:"btn"
-        },
-        {
-            title:'找土地',
-            link:'#bottom',
-            style:"btn"
-        }
-      ],
-
-    }
-  })
-
-
-
-  var app = new Vue({
-    // 標題按鈕的Vue
-    el: '#bottom',
-    data:{
-      linkPressWords:'想看更多嗎?',
-      Icon:
-      [
-        {
-          name:'Facebook',
-          link:'https://www.facebook.com/groups/330793587320368',
-          src:'materials/icons/facebook_1384021.png',
-          display:true
-        },
-        {
-          name:'Instagram',
-          link:'',
-          src:'materials/icons/instagram_1384031.png',
-          display:false
-        },
-        {
-          name:'Youtube',
-          link:'https://www.youtube.com/channel/UCj9u9U6SbGLZZRZ86q3ZEhA',
-          src:'materials/icons/youtube_1384028.png',
-          display:true
-        },
-        {
-          name:'Tiktok',
-          link:'https://www.tiktok.com/@yachinchen?_d=secCgYIASAHKAESPgo85u0d3xwkHv7q7lU3fc07YeF7XZk1UNmX9PLUhtMqmlZlO3SYW70CzFjHusjY%2Bln5aKwsSvM2ElmMI6yuGgA%3D&object_id=7177655001404376069&page_open_method=scan_code&schema_type=4&sec_uid=MS4wLjABAAAAiGmyrasyY2_Aj6H50INmY2WIK8EDt-44AWjQ79zZcd7EWIeen21eS_GqhmKPXUgE&share_app_id=1180&share_author_id=7177655001404376069&share_uid=7177655001404376069&tt_from=scan_code&utm_campaign=client_scan_code&utm_medium=1&utm_source=scan_code&_r=1',
-          src:'materials/icons/tik-tok_3046120.png',
-          display:true
-        },
-      ]
-
-    }
-  })
-
+  // 標題按鈕的Vue
+  el: "#linkList",
+  data: {
+    text: "testing",
+    linkButton: [
+      {
+        title: "關於我",
+        link: "AboutMe.html",
+        style: "btn link-active",
+      },
+      {
+        title: "找租屋",
+        link: "#bottom",
+        style: "btn",
+      },
+      {
+        title: "找房子",
+        link: "#bottom",
+        style: "btn",
+      },
+      {
+        title: "找土地",
+        link: "#bottom",
+        style: "btn",
+      },
+    ],
+  },
+});
 
 var app = new Vue({
   // 標題按鈕的Vue
-  el: '#middle',
-  data:{
-    name:'陳亞琴',
+  el: "#bottom",
+  data: {
+    linkPressWords: "想看更多嗎?",
+    Icon: [
+      {
+        name: "Facebook",
+        link: "https://www.facebook.com/groups/330793587320368",
+        src: "materials/icons/facebook_1384021.png",
+        display: true,
+      },
+      {
+        name: "Instagram",
+        link: "",
+        src: "materials/icons/instagram_1384031.png",
+        display: false,
+      },
+      {
+        name: "Youtube",
+        link: "https://www.youtube.com/channel/UCj9u9U6SbGLZZRZ86q3ZEhA",
+        src: "materials/icons/youtube_1384028.png",
+        display: true,
+      },
+      {
+        name: "Tiktok",
+        link: "https://www.tiktok.com/@yachinchen?_d=secCgYIASAHKAESPgo85u0d3xwkHv7q7lU3fc07YeF7XZk1UNmX9PLUhtMqmlZlO3SYW70CzFjHusjY%2Bln5aKwsSvM2ElmMI6yuGgA%3D&object_id=7177655001404376069&page_open_method=scan_code&schema_type=4&sec_uid=MS4wLjABAAAAiGmyrasyY2_Aj6H50INmY2WIK8EDt-44AWjQ79zZcd7EWIeen21eS_GqhmKPXUgE&share_app_id=1180&share_author_id=7177655001404376069&share_uid=7177655001404376069&tt_from=scan_code&utm_campaign=client_scan_code&utm_medium=1&utm_source=scan_code&_r=1",
+        src: "materials/icons/tik-tok_3046120.png",
+        display: true,
+      },
+    ],
+  },
+});
+
+var app = new Vue({
+  // 標題按鈕的Vue
+  el: "#middle",
+  data: {
+    name: "❤️陳亞琴",
+    lineId: "cyc1666",
 
     // photo:{
     //   profile:{src:'123'}
     // }
-    
-    photo:{
-      profilePhoto:{name:'個人照片' ,src:'123'},
-      line:{line:'LINE',src:'materials/Logo/LINE.png'}
-    }
-      
 
-  }
-})
+    photo: {
+      profilePhoto: { name: "個人照片", src: "/materials/photos/Yachin.jpg" },
+      bgc: { name: "美麗的背景", src: "/materials/photos/鄉村背景.jpg" },
+      line: { line: "LINE", src: "materials/Logo/LINE.png" },
+    },
+  },
+});
