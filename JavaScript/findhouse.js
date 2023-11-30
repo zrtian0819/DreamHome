e394442de16b5999bc8b381faeea6006d8aff420
Joe_Zhang <cjt3399@gmail.com>
2023-12-01 00:14:21 +0800

    新增篩選功能

--- JavaScript/findhouse.js
+++ JavaScript/findhouse.js
@@ -2,8 +2,11 @@
 navLink._data.linkButton[1].style = "btn link-active";
 
 var app = new Vue({
-  el: "#objcards",
+  el: "#objectRegion",
   data: {
+    budget: 10000,
+    keyWords: "",
+    filterMode: false,
     cards: [
       {
         objName: "竹圍海別墅",
@@ -13,8 +16,9 @@ var app = new Vue({
         address: "",
         objType: "透天別墅",
         spaceDiv: "三房兩廳一衛浴",
-        price: "1025",
+        price: 1025,
         discount: false,
+        discussion: false,
       },
       {
         objName: "森林系別墅",
@@ -23,8 +27,9 @@ var app = new Vue({
         address: "",
         objType: "透天別墅",
         spaceDiv: "三房兩廳一衛浴",
-        price: "794",
+        price: 794,
         discount: false,
+        discussion: true,
       },
       {
         objName: "荒郊寧靜別墅",
@@ -34,8 +39,9 @@ var app = new Vue({
         address: "",
         objType: "透天別墅",
         spaceDiv: "三房兩廳一衛浴",
-        price: "866",
+        price: 866,
         discount: true,
+        discussion: false,
       },
       {
         objName: "荒郊寧靜別墅2",
@@ -45,20 +51,35 @@ var app = new Vue({
         address: "",
         objType: "透天別墅",
         spaceDiv: "三房兩廳一衛浴",
-        price: "866",
+        price: 866,
         discount: false,
+        discussion: false,
       },
       {
         objName: "荒郊寧靜別墅3",
         photoLink:
-          "https://www.howlawyer.com.tw/wp-content/uploads/2019/07/joint-venture-advocate-success-win-the-case-return-s.jpg",
-        city: "觀音區",
+          "https://images.homify.com/c_fill,f_auto,q_0,w_740/v1508677797/p/photo/image/2283199/DSC_2122.jpg",
+        city: "平鎮區",
         address: "",
         objType: "透天別墅",
-        spaceDiv: "三房兩廳一衛浴",
-        price: "759",
+        spaceDiv: "五房四廳二衛浴",
+        price: 2099,
         discount: true,
+        discussion: false,
       },
     ],
+  },
+  methods: {
+    openfilter() {
+      filterR = this.$refs.filterRegion;
+      // console.log(filterR);
+      filterR.classList.add("showFilter");
+      // filterR.scrollIntoView();
+      window.scrollTo(top);
+    },
+    closefilter() {
+      filterR = this.$refs.filterRegion;
+      filterR.classList.remove("showFilter");
+    },
   },
 });
